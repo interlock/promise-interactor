a92c0299f362e0482839758cbdc0937ed2c42aa8
James Sapara <james.sapara@gmail.com>
2017-05-03 09:29:03 -0700

    updated code to match lint rules, fixed some obvious misused JS

--- src/interactor.js
+++ src/interactor.js
@@ -1,5 +1,3 @@
-const EventEmitter = require('events');
-
 const Promise = require('bluebird');
 
 const resolveSym = Symbol('resolve');
@@ -15,95 +13,95 @@ class Interactor {
    * should live.
    * @param {object} context
    */
-  constructor(context) {
-    this.context = context || {};
-    this.promise = null;
-    this._state = 'NEW';
-    this[resolveSym] = null;
-    this[rejectSym] = null;
-  }
+    constructor(context) {
+        this.context = context || {};
+        this.promise = null;
+        this._state = 'NEW';
+        this[resolveSym] = null;
+        this[rejectSym] = null;
+    }
 
   /**
    * A static shortcut to creating an instance and calling exec.
-   * @param {object} context 
+   * @param {object} context
    * @returns {Interactor}
    */
-  static exec(context) {
-    const instance = new this(context);
-    return instance.exec();
-  }
+    static exec(context) {
+        const instance = new this(context);
+        return instance.exec();
+    }
 
   /**
    * Run this interactor
    * @returns {Interactor}
    */
-  exec() {
-    this.promise = new Promise((resolve, reject) => {
-      this[resolveSym] = resolve;
-      this[rejectSym] = reject;
-      let root = Promise.resolve();
-      if (typeof this.before === 'function') {
-        this._state = 'BEFORE';
-        const beforePromise = this.before();
-        if (beforePromise instanceof Promise === true) {
-          this.root = beforePromise;
-        }
-      }
-      root.then(() => {
-        this._state = 'CALL';
-        this.call();
-      }).catch((err) => {
-        this.reject(err);
-      });
+    exec() {
+        this.promise = new Promise((resolve, reject) => {
+            this[resolveSym] = resolve;
+            this[rejectSym] = reject;
+            let root = Promise.resolve();
+            if (typeof this.before === 'function') {
+                this._state = 'BEFORE';
+                const beforePromise = this.before();
+                if (beforePromise instanceof Promise === true) {
+                    this.root = beforePromise;
+                }
+            }
+            root.then(() => {
+                this._state = 'CALL';
+                this.call();
+            }).catch((err) => {
+                this.reject(err);
+            });
 
-    });
-    return this.promise;
-  }
+        });
+        return this.promise;
+    }
 
   /**
    * Abstract method that should be implemented in the child
    */
-  call() {
-    throw new Error('Interactor requires call to be implemented');
-  }
+    call() {
+        throw new Error('Interactor requires call to be implemented');
+    }
 
   /**
    * Access to the promise resolve, should be called from call() on success
    */
-  resolve() {
-    if (typeof this.after === 'function') {
-      this._state = 'AFTER';
-      const afterPromise = this.after();
-      if (afterPromise instanceof Promise === true) {
-        afterPromise.then(() => {
-          this._state = 'RESOLVED';
-          this[resolveSym](this);
-        }).catch(this.reject);
-        return;
-      }
+    resolve() {
+        if (typeof this.after === 'function') {
+            this._state = 'AFTER';
+            const afterPromise = this.after();
+            if (afterPromise instanceof Promise === true) {
+                afterPromise.then(() => {
+                    this._state = 'RESOLVED';
+                    this[resolveSym](this);
+                }).catch(this.reject);
+                return;
+            }
+        }
+        this._state = 'RESOLVED';
+        this[resolveSym](this);
     }
-    this._state = 'RESOLVED';
-    this[resolveSym](this);
-  }
 
   /**
    * Access to the promise reject, should be called from call() on failure
    * Also, optionally calls instance method rollback it defined.
-   * @param {*} err 
+   * @param {*} err
    */
-  reject(err) {
-    if (typeof this.rollback === 'function' && this._state == 'CALL') {
-      const rollbackPromise = this.rollback();
-      if (rollbackPromise instanceof Promise === true) {
-        rollbackPromise.finally(() => {
-          this[rejectSym](err);
-        });
-        return; // early exit
-      }
+    reject(err) {
+        if (typeof this.rollback === 'function' && this._state == 'CALL') {
+            const rollbackPromise = this.rollback();
+            if (rollbackPromise instanceof Promise === true) {
+                rollbackPromise.finally(() => {
+                    this[rejectSym](err);
+                });
+                return; // early exit
+            }
+        }
+        this._state = 'REJECTED';
+        this[rejectSym](err);
     }
-    this._state = 'REJECTED';
-    this[rejectSym](err);
-  }
 }
 
 module.exports = Interactor;
