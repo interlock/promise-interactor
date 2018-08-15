55aff40429458a8c20b909eca6210dc35bfbcd23
James Sapara <james.sapara@gmail.com>
2018-08-15 13:15:29 -0700

    first revision to TypeScript

---
+++ src/states.ts
@@ -0,0 +1,11 @@
+/* States symbols, because why not */
+export enum States {
+  UNKNOWN = 0,
+  NEW = 1,
+  BEFORE = 2,
+  CALL = 3,
+  AFTER = 4,
+  REJECTED = 5,
+  ROLLBACK = 6,
+  RESOLVED = 7,
+}
