7c5e63fc20bc558285a046fde14dcee325d56c59
interlock <interlock@users.noreply.github.com>
2020-08-07 15:38:30 -0700

    removed dist

--- dist/organizer.d.ts
+++
@@ -1,9 +0,0 @@
-import { Interactor, IRollback, interactorConstructor } from './interactor';
-export declare abstract class Organizer<T extends object = {}> extends Interactor<T> implements IRollback {
-    private currentInteractorIndex;
-    constructor(context: T);
-    abstract organize(): Array<interactorConstructor<T>>;
-    createInteractor(i: interactorConstructor<T>): Interactor<T>;
-    exec(): Promise<this>;
-    rollback(): Promise<void> | Promise<{}>;
-}
