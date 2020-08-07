9a3025b9127d5995a5a3a8e3fd572833cef0f7f3
June S <interlock@users.noreply.github.com>
2020-08-07 11:25:27 -0700

    Remove dist folder (#56)
    * this does not need to be in repo anymore
    * ignore dist

--- dist/promise.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Determine the least amount of functionality to be a valid promise
- * @arg {*} The promise in question
- * @return {boolean}
- */
-export declare function isPromise<T>(p: any): p is Promise<T>;
-export declare type IResolve = (value?: {} | PromiseLike<{}> | undefined) => void;
-export declare type IReject = (reason: any) => void;
