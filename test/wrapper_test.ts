e453cf6b835651def62390467a9a2f637a0d4499
June S <interlock@users.noreply.github.com>
2020-08-14 09:37:16 -0700

    Addition interactorConditional wrapper (#61)
    * interactorConditional wrapper
    
    * fixed lint issues
    
    * Revered array

--- test/wrapper_test.ts
+++ test/wrapper_test.ts
@@ -1,7 +1,6 @@
 import chai from 'chai';
 import spies from 'chai-spies'; // TODO refactor in to test helper
-import { IAfter, IBefore, Interactor, IRollback, States as states, interactorWrapper, Organizer } from '../src';
-import { Test } from 'mocha';
+import { Interactor, interactorWrapper, interactorConditional, Organizer } from '../src';
 
 chai.use(spies);
 const expect = chai.expect;
@@ -43,115 +42,130 @@ class TestOrganizer extends Organizer<IOrgContext> {
   }
 }
 
-describe('wrapper', () => {
+describe('wrappers', () => {
+  describe('interactorWrapper', () => {
+    it('wrap functions', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2
+      };
 
-  it('wrap functions', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2
-    };
-
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
-      return { catTreat: context.catTreat || '' }
-    }, (context, origContext) => {
-      origContext.catHappy = context.catHappy;
-    });
-    const inst = new c(orgCtx);
-    await inst.exec();
-    expect(inst.context.catHappy).to.be.true;
-  });
-
-  it('default unwrap does assign', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2
-    };
-
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
-      return { catTreat: context.catTreat || '' }
-    });
-    const inst = new c(orgCtx);
-    await inst.exec();
-    expect(inst.context.catHappy).to.be.true;
-  });
-
-  it('default wrap', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2,
-    };
-
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor);
-    const inst = new c(orgCtx);
-    await inst.exec();
-    expect(inst.context.catHappy).to.be.true;
-  });
-
-  it('catches exception in wrap', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2
-    };
-
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
-      throw new Error('kitten overload');
-    }, (context, origContext) => {
-      origContext.catHappy = context.catHappy;
-    });
-    const inst = new c(orgCtx)
-    try {
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
+        return { catTreat: context.catTreat || '' }
+      }, (context, origContext) => {
+        origContext.catHappy = context.catHappy;
+      });
+      const inst = new c(orgCtx);
       await inst.exec();
-    } catch (err) {
-      expect(err.message).to.be.equal('kitten overload');
-    }
-  });
-
-  it('catches exception in unwrap', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2
-    };
-
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
-      return { catTreat: context.catTreat || '' }
-    }, (context, origContext) => {
-      throw new Error('Cat refuses to unbox');
+      expect(inst.context.catHappy).to.be.true;
     });
-    const inst = new c(orgCtx)
-    try {
+
+    it('default unwrap does assign', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2
+      };
+
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
+        return { catTreat: context.catTreat || '' }
+      });
+      const inst = new c(orgCtx);
       await inst.exec();
-    } catch (err) {
-      expect(err.message).to.be.equal('Cat refuses to unbox');
-    }
-  });
+      expect(inst.context.catHappy).to.be.true;
+    });
 
-  it('rejects if wrapped interactor throws', (done) => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'kibble',
-      pew: 2,
-    };
+    it('default wrap', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2,
+      };
 
-    const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor);
-    const inst = new c(orgCtx);
-    inst.exec().catch((err) => {
-      expect(err.message).to.be.eq('Unacceptable Cat treat!');
-      done();
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor);
+      const inst = new c(orgCtx);
+      await inst.exec();
+      expect(inst.context.catHappy).to.be.true;
+    });
+
+    it('catches exception in wrap', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2
+      };
+
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
+        throw new Error('kitten overload');
+      }, (context, origContext) => {
+        origContext.catHappy = context.catHappy;
+      });
+      const inst = new c(orgCtx)
+      try {
+        await inst.exec();
+      } catch (err) {
+        expect(err.message).to.be.equal('kitten overload');
+      }
+    });
+
+    it('catches exception in unwrap', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2
+      };
+
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor, (context) => {
+        return { catTreat: context.catTreat || '' }
+      }, (context, origContext) => {
+        throw new Error('Cat refuses to unbox');
+      });
+      const inst = new c(orgCtx)
+      try {
+        await inst.exec();
+      } catch (err) {
+        expect(err.message).to.be.equal('Cat refuses to unbox');
+      }
+    });
+
+    it('rejects if wrapped interactor throws', (done) => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'kibble',
+        pew: 2,
+      };
+
+      const c = interactorWrapper<IOrgContext, ITestContext>(TestInteractor);
+      const inst = new c(orgCtx);
+      inst.exec().catch((err) => {
+        expect(err.message).to.be.eq('Unacceptable Cat treat!');
+        done();
+      })
+    });
+    it('can be included with other interactors', async () => {
+      const orgCtx: IOrgContext = {
+        catName: 'Nimh',
+        catTreat: 'fish',
+        pew: 2,
+      };
+      const org = await TestOrganizer.exec(orgCtx);
+      expect(org.context.catHappy).to.be.true;
+      expect(org.context.pew).to.be.eq(4);
     })
-  });
-  it('can be included with other interactors', async () => {
-    const orgCtx: IOrgContext = {
-      catName: 'Nimh',
-      catTreat: 'fish',
-      pew: 2,
-    };
-    const org = await TestOrganizer.exec(orgCtx);
-    expect(org.context.catHappy).to.be.true;
-    expect(org.context.pew).to.be.eq(4);
   })
+
+  describe('interactorConditional', () => {
+    it('calls interactor if true', async () => {
+      const interactor = interactorConditional(TestInteractor2, (ctx) => ctx.pew === 2);
+      const inst = await (new interactor({ pew: 2 }).exec());
+      expect(inst.context.pew).to.eq(4);
+    });
+
+    it('does not call interactor if false', async () => {
+      const interactor = interactorConditional(TestInteractor2, (ctx) => ctx.pew === 2);
+      const inst = await (new interactor({ pew: 1 }).exec());
+      expect(inst.context.pew).to.eq(1);
+    });
+  });
 });
